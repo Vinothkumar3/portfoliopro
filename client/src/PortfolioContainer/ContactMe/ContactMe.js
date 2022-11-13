625ac969fb0ca17b18dfd08081221996b7524cd6
vinothkumar m <gmvinothkumar1998@gmail.com>
2022-11-13 21:02:16 +0530

    complete react resume portfolio

--- client/src/PortfolioContainer/ContactMe/ContactMe.js
+++ client/src/PortfolioContainer/ContactMe/ContactMe.js
@@ -13,7 +13,7 @@ function ContactMe() {
 
     emailjs
       .sendForm(
-        "service_zalcudq",
+        "servicezalcudq",
         "template_j8li8h5",
         form.current,
         "iGs5tg5V0Yz7wLE7k"
